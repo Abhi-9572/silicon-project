ce8946f1c9c2c0e35b7876f6b7968e47a04c45e2
Abhi-9572 <96545773+Abhi-9572@users.noreply.github.com>
2022-11-29 12:16:52 +0530

    changes in framer motion

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,16 +2,11 @@ import React from 'react'
 import './Home.css'
 function Home() {
     function handleScroll(){
-        const im1=document.querySelector('.Home-mid-image');
-        const im2=document.querySelector('.Home_sec_image');
+        const midImage=document.querySelector('.Home-mid-image');
+        const secImage=document.querySelector('.Home_sec_image');
         const scrolled=window.scrollY;
-        console.log(window.getComputedStyle(im2).marginTop,"im2")
-        console.log(window.getComputedStyle(im2).marginTop);
-        im1.style.marginTop=`${90-scrolled*0.2}px`;
-        im2.style.marginTop=`${-179+scrolled*0.3}px`;
-        console.log(scrolled);
-        console.log(im1);
-        
+        midImage.style.marginTop=`${90-scrolled*0.2}px`;
+        secImage.style.marginTop=`${-179+scrolled*0.3}px`;
     }
      
 
@@ -19,25 +14,24 @@ function Home() {
 
   return (
     <div className='Home_section'>
-        <div className="Home_text_section">
-        <div className="Silicon-Heading">
-            <h1>Silicon</h1>
-        </div>
-        <div className="Home_description">
-            <span>Multipurpose Business /
-                Technology Template</span> 
-        </div> 
-        <div className="Home_toggle">
-            <img src="https://silicon.madrasthemes.com/wp-content/uploads/2022/01/switcher.svg" alt="" />
-            
-        </div>
-        
+    <div id='leftFrameMotion'>
+            <span id='leftFrameMotion1'>Silicon</span>
+            <div>
+            <span className='leftFrameMotion2'>Multipurpose Business /</span>
+            <br/>
+            <span className='leftFrameMotion2'>Technology Template</span>
+            </div>
+            <img src='	https://silicon.madrasthemes.com/wp-content/uploads/2022/01/switcher.svg'/>
+            <div className='leftFrameMotion3'>
+            <span class="fa-sharp fa-solid fa-circle-chevron-down"></span>
+            <p>View Landings</p>
+            </div>
         </div>
         <div className="Home-mid-image">
-            <img src={require('../assests/scrollImage.png')} alt="" />
+            <img src={require('../assests/scrollImage1.png')} alt="" />
         </div>
         <div className='Home_sec_image'>
-        <img src={require('../assests/screens02.png')} alt="" />
+        <img src={require('../assests/scrollImage2.png')} alt="" />
         </div>
     </div>
   )
