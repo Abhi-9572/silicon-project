326ec77a5dc0b041f376e58a87d2bd52b176ccd5
Abhi-9572 <96545773+Abhi-9572@users.noreply.github.com>
2022-11-30 10:16:26 +0530

    dropdown

--- src/components/NavRes.js
+++ src/components/NavRes.js
@@ -5,8 +5,44 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Portfolio from './DropDowns/Portfolio';
+import { useState } from 'react';
+import Pages from './DropDowns/Pages';
+import Landings from './DropDowns/Landings';
 
 function NavRes() {
+
+  const [isHoveringland, setIsHoveringland] = useState(false);
+
+  const handleMouseOverland = () => {
+    setIsHoveringland(true);
+  };
+
+  const handleMouseOutland= () => {
+    setIsHoveringland(false);
+  };
+  const [isHoveringport, setIsHoveringport] = useState(false);
+
+  const handleMouseOverport = () => {
+    setIsHoveringport(true);
+  };
+
+  const handleMouseOutport = () => {
+    setIsHoveringport(false);
+  };
+
+  const [isHoveringpage, setIsHoveringpage] = useState(false);
+
+  const handleMouseOverpage = () => {
+    setIsHoveringpage(true);
+  };
+
+  const handleMouseOutpage = () => {
+    setIsHoveringpage(false);
+  };
+
+
+
   return (
     
     <header id="navigation" className="site-header header navbar navbar-expand-lg  fixed-top navbar-light bg-light shadow-sm">
@@ -19,19 +55,22 @@ function NavRes() {
               <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" />
             </div>
             <div className="offcanvas-body">
-              <ul id="menu-landings" className="navbar-nav me-auto mb-2 mb-lg-0"><li id="menu-item-4790" className="dropdown nav-item menu-item-4790"><a title="Landings" href="#" className="dropdown-toggle  nav-link" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="menu-item-dropdown-4790" data-bs-auto-close="false">Landings</a>
-                 
+              <ul  id="menu-landings" className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li onMouseOver={handleMouseOverland} onMouseOut={handleMouseOutland} id="menu-item-4790" className="dropdown nav-item menu-item-4790"><a title="Landings" href="#" className="dropdown-toggle  nav-link" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="menu-item-dropdown-4790" data-bs-auto-close="false">Landings</a>
+              {isHoveringland && (
+          <div className='landingDropdown'><Landings/></div>
+        )}
                 </li>
-                <li id="menu-item-4825" className="dropdown nav-item menu-item-4825"><a title="Pages" href="#" className="dropdown-toggle  nav-link" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="menu-item-dropdown-4825" data-bs-auto-close="false">Pages</a>
-                  
+                <li onMouseOver={handleMouseOverpage} onMouseOut={handleMouseOutpage} id="menu-item-4825" className="dropdown nav-item menu-item-4825"><a title="Pages" href="#" className="dropdown-toggle  nav-link" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="menu-item-dropdown-4825" data-bs-auto-close="false">Pages</a>
+                {isHoveringpage && (
+          <div className='pageDropdown'><Pages/></div>
+        )}
                 </li>
-                <li id="menu-item-8010" className="dropdown nav-item menu-item-8010"><a title="Portfolio" href="#" className="dropdown-toggle  nav-link" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="menu-item-dropdown-8010" data-bs-auto-close="false">Portfolio</a>
-                  <ul className="dropdown-menu " aria-labelledby="menu-item-dropdown-8010" role="menu">
-                    <li id="menu-item-8011" className="p-0 menu-item-8011"><a title="Grid View" href="https://silicon.madrasthemes.com/portfolio-grid/" className=" dropdown-item">Grid View</a></li>
-                    <li id="menu-item-8012" className="p-0 menu-item-8012"><a title="List View" href="https://silicon.madrasthemes.com/portfolio-list/" className=" dropdown-item">List View</a></li>
-                    <li id="menu-item-8013" className="p-0 menu-item-8013"><a title="Slider View" href="https://silicon.madrasthemes.com/portfolio-slider/" className=" dropdown-item">Slider View</a></li>
-                    <li id="menu-item-8014" className="p-0 menu-item-8014"><a title="Single Project" href="https://silicon.madrasthemes.com/portfolio/single-project-3d-rendering-of-human-sculpture/" className=" dropdown-item">Single Project</a></li>
-                  </ul>
+                <li onMouseOver={handleMouseOverport} onMouseOut={handleMouseOutport} id="menu-item-8010" className="dropdown nav-item menu-item-8010"><a title="Portfolio" href="#" className="dropdown-toggle  nav-link" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="menu-item-dropdown-8010" data-bs-auto-close="false">Portfolio</a>
+                  {/* <div className='portfolioDreopdown'><Portfolio/></div> */}
+                  {isHoveringport && (
+          <div className='portfolioDreopdown'><Portfolio/></div>
+        )}
                 </li>
                 <li id="menu-item-9872" className="nav-item menu-item-9872"><a title="Support" target="_blank" href="https://themeforest.net/user/madrasthemes" className=" nav-link">Support</a></li>
                 <li id="menu-item-9873" className="nav-item menu-item-9873"><a title="Docs" target="_blank" href="https://docs.madrasthemes.com/silicon/" className=" nav-link">Docs</a></li>
